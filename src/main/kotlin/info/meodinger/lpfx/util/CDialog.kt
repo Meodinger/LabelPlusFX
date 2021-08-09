9c894490b40b7089b9784c89a46bb420ea3c1f2f
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2021-08-09 17:36:05 +0800

    CDialog refactor: init

--- src/main/kotlin/info/meodinger/lpfx/util/CDialog.kt
+++ src/main/kotlin/info/meodinger/lpfx/util/CDialog.kt
@@ -3,219 +3,94 @@ package info.meodinger.lpfx.util.dialog
 import info.meodinger.lpfx.util.resource.I18N
 import info.meodinger.lpfx.util.resource.get
 
-import javafx.event.ActionEvent
 import javafx.event.EventHandler
-import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.control.*
-import javafx.scene.layout.GridPane
-import javafx.scene.layout.HBox
-import javafx.scene.layout.VBox
+import javafx.scene.layout.*
 import javafx.stage.Window
 import javafx.util.Callback
-import java.io.PrintWriter
-import java.io.StringWriter
 import java.util.*
-
 
 /**
  * Author: Meodinger
- * Date: 2021/7/29
+ * Date: 2021/8/8
  * Location: info.meodinger.lpfx.util
  */
-private val dialog: Dialog<ButtonType> = Dialog()
 
-private fun initDialog() {
-    dialog.title = ""
-    dialog.contentText = ""
-    dialog.dialogPane = DialogPane()
-}
+// Common dialogs
+private val confirmDialog = Dialog<ButtonType>()
+private val infoDialog = Dialog<ButtonType>()
+private val alertDialog = Dialog<ButtonType>()
+private val errorDialog = Dialog<ButtonType>()
+private val exceptionDialog = Dialog<ButtonType>()
 
-fun initCDialogOwner(window: Window?) {
-    dialog.initOwner(window)
-}
-
-/**
- * Show stack trace in expandable content
- * @param e Exception to print
- * @return ButtonType.OK?
- */
-fun showException(e: Exception): Optional<ButtonType> {
-
-    // Create expandable Exception.
-    val sw = StringWriter()
-    val pw = PrintWriter(sw)
-    e.printStackTrace(pw)
-    val exceptionText = sw.toString()
-
-    val expContent = GridPane()
-    val label = Label("The exception stacktrace is:")
-    val textArea = TextArea(exceptionText)
-    textArea.isEditable = false
-    textArea.prefWidthProperty().bind(expContent.widthProperty())
-    textArea.prefHeightProperty().bind(expContent.heightProperty())
-    expContent.add(label, 0, 0)
-    expContent.add(textArea, 0, 1)
-    expContent.prefHeight = 400.0
-    expContent.prefWidth = 800.0
-
-    initDialog()
-    dialog.title = I18N["common.error"]
-    dialog.headerText = e.javaClass.toString()
-    dialog.contentText = e.message
-    dialog.dialogPane.buttonTypes.add(ButtonType.OK)
-    dialog.dialogPane.expandableContent = expContent
-    return dialog.showAndWait()
+fun init(owner: Window?) {
+    confirmDialog.initOwner(owner)
+    infoDialog.initOwner(owner)
+    alertDialog.initOwner(owner)
+    errorDialog.initOwner(owner)
+    exceptionDialog.initOwner(owner)
 }
 
 /**
- * Show error
- * @param content Error to show
- * @return ButtonType? with ButtonDate OK
+ * Show message for confirm
+ * @param content Message to show
+ * @return ButtonType? YES | NO
  */
-fun showError(content: String): Optional<ButtonType> {
-    return showError(I18N["common.error"], content)
+fun showConfirm(content: String): Optional<ButtonType> {
+    return showConfirm(I18N["common.confirm"], null, content)
 }
-/**
- * Show error with specific title
- * @param title Dialog title
- * @param content Error to show
- * @return ButtonType? with ButtonDate OK
- */
-fun showError(title: String, content: String): Optional<ButtonType> {
-    initDialog()
-    dialog.title = title
-    dialog.contentText = content
-    dialog.dialogPane.buttonTypes.add(ButtonType.OK)
-    return dialog.showAndWait()
+fun showConfirm(title: String, header: String?, content: String): Optional<ButtonType> {
+    return confirmDialog.showAndWait()
 }
 
 /**
  * Show information
  * @param content Info to show
- * @return ButtonType.OK?
+ * @return ButtonType? OK
  */
 fun showInfo(content: String): Optional<ButtonType> {
-    return showInfo(I18N["common.info"], content)
+    return showInfo(I18N["common.confirm"], null, content)
 }
-/**
- * Show information with specific title
- * @param title Dialog title
- * @param content Info to show
- * @return ButtonType.OK?
- */
-fun showInfo(title: String, content: String): Optional<ButtonType> {
-    initDialog()
-    dialog.title = title
-    dialog.contentText = content
-    dialog.dialogPane.buttonTypes.add(ButtonType.OK)
-    return dialog.showAndWait()
-}
-/**
- * Show information with specific title and link
- * @param title Dialog title
- * @param content Info to show
- * @param linkText Text for hyperlink
- * @param handler Handler for action of link
- * @return ButtonType.OK?
- */
-fun showInfoWithLink(title: String, content: String, linkText: String, handler: EventHandler<ActionEvent>): Optional<ButtonType> {
-    initDialog()
-    dialog.title = title
-    dialog.dialogPane.buttonTypes.add(ButtonType.OK)
-
-    val label = Label(content)
-    val separator = Separator()
-    val hyperlink = Hyperlink(linkText)
-    separator.padding = Insets(8.0, 0.0, 8.0, 0.0)
-    hyperlink.onAction = handler
-
-    dialog.dialogPane.content = VBox(label, separator, hyperlink)
-
-    return dialog.showAndWait()
+fun showInfo(title: String, header: String?, content: String): Optional<ButtonType> {
+    return infoDialog.showAndWait()
 }
 
 /**
  * Show alert
  * @param content Alert to show
- * @return ButtonType? with ButtonDate YES | NO | CANCEL
+ * @return ButtonType? YES | NO | CANCEL
  */
 fun showAlert(content: String): Optional<ButtonType> {
-    return showAlert(I18N["common.alert"], content)
+    return showAlert(I18N["common.confirm"], null, content)
 }
-/**
- * Show alert with specific title
- * @param title Dialog title
- * @param content Alert to show
- * @return ButtonType? with ButtonDate YES | NO | CANCEL
- */
-fun showAlert(title: String, content: String): Optional<ButtonType> {
-    return showAlert(title, content, I18N["common.yes"], I18N["common.no"])
-}
-/**
- * Show alert with specific title and yes/no text
- * @param title Dialog title
- * @param content Alert to show
- * @param yes Text for ButtonType with ButtonData.YES
- * @param no  Text for ButtonType with ButtonData.NO
- * @return ButtonType? with ButtonDate YES | NO | CANCEL
- */
-fun showAlert(title: String, content: String, yes: String, no: String): Optional<ButtonType> {
-    initDialog()
-    dialog.title = title
-    dialog.contentText = content
-    dialog.dialogPane.buttonTypes.addAll(
-        ButtonType(yes, ButtonBar.ButtonData.YES),
-        ButtonType(no, ButtonBar.ButtonData.NO),
-        ButtonType(I18N["common.cancel"], ButtonBar.ButtonData.CANCEL_CLOSE)
-    )
-    return dialog.showAndWait()
+fun showAlert(title: String, header: String?, content: String): Optional<ButtonType> {
+    return alertDialog.showAndWait()
 }
 
 /**
- * Show message for confirm
- * @param msg Message to show
- * @return ButtonType?.YES | NO
+ * Show error
+ * @param content Error to show
+ * @return ButtonType? OK
  */
-fun showConfirm(msg: String): Optional<ButtonType> {
-    return showConfirm(I18N["common.confirm"], msg)
+fun showError(content: String): Optional<ButtonType> {
+    return showError(I18N["common.confirm"], null, content)
 }
-/**
- * Show message for confirm with specific title
- * @param title Dialog title
- * @param msg Message to show
- * @return ButtonType?.YES | NO
- */
-fun showConfirm(title: String, msg: String): Optional<ButtonType> {
-    return showConfirm(title, "", msg)
-}
-/**
- * Show message for confirm
- * @param title Dialog title
- * @param header Dialog header text
- * @param msg Message to show
- * @return ButtonType?.YES | NO
- */
-fun showConfirm(title: String, header: String, msg: String): Optional<ButtonType> {
-    initDialog()
-    dialog.title = title
-    dialog.headerText = header
-    dialog.contentText = msg
-    dialog.dialogPane.buttonTypes.addAll(ButtonType.YES, ButtonType.NO)
-    return dialog.showAndWait()
+fun showError(title: String, header: String?, content: String): Optional<ButtonType> {
+    return errorDialog.showAndWait()
 }
 
 /**
- * For specific usage
+ * Show stack trace in expandable content
+ * @param e Exception to print
+ * @return ButtonType? OK
  */
-
-fun <T> showChoice(owner: Window?, title: String, header: String, choices: List<T>): Optional<T> {
-    val dialog = ChoiceDialog(choices[0], choices)
-    dialog.initOwner(owner)
-    dialog.title = title
-    dialog.contentText = header
-    return dialog.showAndWait()
+fun showException(e: Exception): Optional<ButtonType> {
+    return exceptionDialog.showAndWait()
 }
+
+
+// Specific dialogs
 
 fun showInput(owner: Window?, title: String, header: String, placeholder: String?, formatter: TextFormatter<String>?): Optional<String> {
     val dialog = TextInputDialog(placeholder)
@@ -239,6 +114,14 @@ fun showInputArea(owner: Window?, title: String, placeholder: String): Optional<
         if (buttonType == ButtonType.OK) textArea.text
         else placeholder
     }
+    return dialog.showAndWait()
+}
+
+fun <T> showChoice(owner: Window?, title: String, header: String, choices: List<T>): Optional<T> {
+    val dialog = ChoiceDialog(choices[0], choices)
+    dialog.initOwner(owner)
+    dialog.title = title
+    dialog.contentText = header
     return dialog.showAndWait()
 }
 

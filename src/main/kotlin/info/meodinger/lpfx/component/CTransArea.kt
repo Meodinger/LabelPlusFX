ecb282ff1e2d241a4ae048e3c272df141ca6054c
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2021-08-16 17:40:22 +0800

    comp refactor

---
+++ src/main/kotlin/info/meodinger/lpfx/component/CTransArea.kt
@@ -0,0 +1,17 @@
+package info.meodinger.lpfx.component
+
+import javafx.scene.control.TextArea
+
+/**
+ * Author: Meodinger
+ * Date: 2021/8/16
+ * Location: info.meodinger.lpfx.component
+ */
+class CTransArea: TextArea() {
+
+    init {
+
+    }
+
+    
+}
---
+++ src/main/kotlin/info/meodinger/lpfx/component/CTreeView.kt
@@ -0,0 +1,15 @@
+package info.meodinger.lpfx.component
+
+import javafx.scene.control.TreeView
+
+/**
+ * Author: Meodinger
+ * Date: 2021/8/16
+ * Location: info.meodinger.lpfx.component
+ */
+class CTreeView: TreeView<String>() {
+
+    init {
+
+    }
+}

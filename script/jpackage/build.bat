b2d3fe99ca49a007ec8621b7a80918810cfcf578
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2022-03-11 08:44:51 +0800

    build script update

--- script/jpackage/build.bat
+++ script/jpackage/build.bat
@@ -1,11 +1,10 @@
 @echo off
 
 set DIR=%~dp0..\..
-rd /S /Q "%DIR%\out\LabelPlusFX"
-del /F /Q "%DIR%\out\*.zip"
+rd /S /Q ".\LabelPlusFX"
 
 set OUT="%DIR%\out"
 set MODULES="%DIR%\out\artifacts\lpfx_jar"
 set ICON="%DIR%\images\icons\cat.ico"
 
-jpackage --verbose --type app-image --name LabelPlusFX --icon %ICON% --dest . --module-path %MODULES% --module lpfx/ink.meodinger.lpfx.LauncherKt
+jpackage --verbose --type app-image --name LabelPlusFX --icon %ICON% --dest . --module-path %MODULES% --add-modules jdk.crypto.cryptoki --module lpfx/ink.meodinger.lpfx.LauncherKt
--- script/jpackage/build.sh
+++ script/jpackage/build.sh
@@ -1,9 +1,9 @@
 DIR=$PWD/../..
 
-rm -rf "$DIR/out/LabelPlusFX"
+rm -rf "./LabelPlusFX"
 
 OUT="$DIR/out"
 MODULES="$DIR/out/artifacts/lpfx_jar"
 ICON="$DIR/images/icons/cat.icns"
 
-jpackage --verbose --type app-image --name LabelPlusFX --icon $ICON --dest . --module-path $MODULES --module lpfx/ink.meodinger.lpfx.LauncherKt
+jpackage --verbose --type app-image --name LabelPlusFX --icon $ICON --dest . --module-path $MODULES --add-modules jdk.crypto.cryptoki --module lpfx/ink.meodinger.lpfx.LauncherKt
